8d8d9e20b8fc73f39b95a65037714b3da04de147
Tejas Dixit <tejasd910@gmail.com>
2026-03-08 02:04:49 +0530

    Atlas connection

--- 05project/src/controller/user.controller.js
+++ 05project/src/controller/user.controller.js
@@ -2,9 +2,21 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+    //get user details from frontEnd
+    //validation - not empty input
+    //check if user already exist - (email/username)
+    //check from image and avatar
+    //upload them in cloudinary
+    //create user object - create entry in db
+    //remove password and refresh token field from resopnse
+    //check for user creation
+    //return response
+
+    const {fullNme, email, username, password}= req.body
+    console.log("email :", email);
+    
 })
+
+
 
 export { registerUser }
