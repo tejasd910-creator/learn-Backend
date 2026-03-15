2b25640fbdf6dfffafbca9e46892b95f69476b99
Tejas Dixit <tejasd910@gmail.com>
2026-03-15 23:50:21 +0530

    Login and Logout User

--- 05project/src/controller/user.controller.js
+++ 05project/src/controller/user.controller.js
@@ -4,7 +4,25 @@ import { User } from "../models/user.model.js"
 import { uploadCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+//creating access and refresh token method (easy for code reusability)
 
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findOne(userId)             // get user data
+        const accessToken = user.generateAccessToken()      // generate accessToken
+        const refreshToken = user.generateRefreshToken()    // generate refreshToken
+
+        user.refreshToken = refreshToken                    // adding refreshToken  to the user data
+        await user.save({ validateBeforeSave: false })        // don't validate data before saving it in the database(I know what i'm doing and don't cause password issue)
+
+        return { accessToken, refreshToken }                  // return both tokens
+
+    } catch (error) {
+        throw new ApiError(500, "Somethig went wrong while generating access and refresh tokens")
+    }
+}
+
+//REGISTER USER
 const registerUser = asyncHandler(async (req, res) => {
     //get user details from frontEnd
     //validation - not empty input
@@ -13,7 +31,7 @@ const registerUser = asyncHandler(async (req, res) => {
     //upload them in cloudinary
     //create user object - create entry in db
     //remove password and refresh token field from resopnse
-    //check for user creation
+    //check for user creation 
     //return response
 
     const { fullName, email, username, password } = req.body
@@ -50,17 +68,17 @@ const registerUser = asyncHandler(async (req, res) => {
     // req.body  method by express give access to content of file(default)
 
     //multer give req.files access to access files (? == if exist)
-    //console.log(req.files);
-    
+    // console.log(req.files);
+
     const avatarLocalPath = req.files?.avatar[0]?.path;         // give the local path of the file that exist in the system
     // const coverImageLocalPath = req.files?.coverImage[0]?.path;         // give the local path of the file that exist in the system
 
     let coverImageLocalPath;
-    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
     }
 
-    if(!avatarLocalPath){
+    if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
     }
 
@@ -68,10 +86,12 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const avatar = await uploadCloudinary(avatarLocalPath)           // upload hone mai time lage ga so await
     const coverImage = await uploadCloudinary(coverImageLocalPath)
+    // console.log(avatar);
+
 
     //CHECK IF UPLOADED SUCCESSFULLY
 
-    if(!avatarLocalPath){
+    if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
     }
 
@@ -92,7 +112,7 @@ const registerUser = asyncHandler(async (req, res) => {
         "-password -refreshToken"           // select method help to decide which field need not to be check for user checking "-" indicate this field neednot ti be checked
     )
 
-    if(!createdUser){
+    if (!createdUser) {
         throw new ApiError(500, "Something went wrong while registering a user")
     }
 
@@ -103,6 +123,101 @@ const registerUser = asyncHandler(async (req, res) => {
     )
 })
 
+//LOGIN USER
+const loginUser = asyncHandler(async (req, res) => {
+    // take data from req.body 
+    // username or email (how to login user)
+    // find the user
+    // password check
+    // generate access and referesh token
+    // send tem through cookie
+
+    //GETTING DATA
+    const { email, username, password } = req.body
+
+    //CHECKING FOR REQUIRED INPUT
+    if (!username || !email) {
+        throw new ApiError(400, "username or email is required")
+    }
+
+    //FINDING USER IN DATABASE
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    //ERROR IF NOT EXIST
+    if (!user) {
+        throw new ApiError(404, "User doesn't exist")
+    }
+
+    //PASSWORD CHECK
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid User credentials")
+    }
+
+    //TOKEN GENERATION
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    //since above user don't have any token right now so again call database
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {                 //an Object used to set some restriction
+        httpOnly: true,               // By default any one can access and modified cookies but after httpOnly and secure is true only server can modified cookies
+        secure: true
+    }
+  
+    //COOKIE RESPONSE
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)           // method to directly send cookie(key, value, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(                                                 // json response
+        new ApiResponse(                                   // response api
+            200,                                           // status code
+            {
+                user: loggedInUser, accessToken, refreshToken                 // data if frontEnd want to access tokens
+            },
+            "User logged in Successfully"                  // message
+        )
+    )
+})
+
+//LOGOUT USER (we are not able to logout user so we created a middleware to do so)
+const logoutUser = asyncHandler(async(req, res) => {              
+    //with the help of verifyJWT middleware now we have user detaild in the req 
+    User.findByIdAndUpdate(                    // method to find and update database
+        req.user._id,                          // find _id from user in req
+        {
+            $set: {                            // operator to update value in the database
+                refreshToken: undefined        // delete refreshToken
+            }
+        },
+        {
+            new: true                          // return new updated value of user after deleting refreshToken
+        }
+    )
+    //delete refreshToken and accessToken 
+
+    const options = {                 //an Object used to set some restriction
+        httpOnly: true,               // By default any one can access and modified cookies but after httpOnly and secure is true only server can modified cookies
+        secure: true
+    }
+  
+    //COOKIE RESPONSE
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)           // method to directly send cookie(key, value, options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"))
+})
 
 
-export { registerUser }
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
--- 05project/src/routes/user.routes.js
+++ 05project/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express"
-import { registerUser } from "../controller/user.controller.js"
+import { loginUser, logoutUser, registerUser } from "../controller/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const userRouter = Router()
 
@@ -18,5 +19,10 @@ userRouter.route("/register").post(
     registerUser
 )
 
+userRouter.route("/login").post(loginUser)
+
+//secured routes
+
+userRouter.route("/logout").post(verifyJWT, logoutUser)            //verifyJWT is s middleware so i.e why next() is used in yhe middleware to tell the next step(first verifyJWt then logoutUser)
 
 export default userRouter
